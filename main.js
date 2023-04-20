e1b440f2499e037deaaab554f5fb21072dc53b4e
D1051594翁家雋 <D1051594@o365.fcu.edu.tw>
2023-04-20 15:37:54 +0800

    add option

--- main.js
+++ main.js
@@ -7,11 +7,35 @@ function changeMode() {
         document.getElementById("inputMode").placeholder="請輸入手機號碼";
 
         let parentDiv = document.getElementById("upon");
-        let AddedThing = document.createElement("select");
-        AddedThing.setAttribute("id", "newSelect");
-        AddedThing.setAttribute("class", "");
+        let MySelect = document.createElement("select");
+        MySelect.setAttribute("id", "newSelect");
+
+        let option1 = document.createElement("option");
+        option1.text="+886";
+        MySelect.add(option1);
+        let option2 = document.createElement("option");
+        option2.text="+852";
+        MySelect.add(option2);
+        let option3 = document.createElement("option");
+        option3.text="+853";
+        MySelect.add(option3);
+        let option4 = document.createElement("option");
+        option4.text="+1";
+        MySelect.add(option4);
+        let option5 = document.createElement("option");
+        option5.text="+7";
+        MySelect.add(option5);
+        let option6 = document.createElement("option");
+        option6.text="+20";
+        MySelect.add(option6);
+        let option7 = document.createElement("option");
+        option7.text="+27";
+        MySelect.add(option7);
+
+
+
         let OldInput = document.getElementById("inputMode");
-        parentDiv.insertBefore(AddedThing, OldInput);
+        parentDiv.insertBefore(MySelect, OldInput);
 
         OldInput.style.width="300px";
 
