b4c06f74c1483fb6b9789bf0f892349c9eb65d6d
User <ericweng92@gmail.com>
2023-04-20 14:16:39 +0800

    add select

--- main.js
+++ main.js
@@ -5,18 +5,27 @@ function changeMode() {
         document.getElementById("signingMode").textContent = "手機號碼";
 
         document.getElementById("inputMode").placeholder="請輸入手機號碼";
-        /*
-        var y = document.getElementsByClassName("upon");
-        var x = document.createElement("select");
-        x.setAttribute("class", "EmailAndPassword");
-        document.body.insertAdjacentElement(y,x);
-         */
+
+        let parentDiv = document.getElementById("upon");
+        let AddedThing = document.createElement("select");
+        AddedThing.setAttribute("id", "newSelect");
+        AddedThing.setAttribute("class", "");
+        let OldInput = document.getElementById("inputMode");
+        parentDiv.insertBefore(AddedThing, OldInput);
+
+        OldInput.style.width="300px";
+
         mode = 2;
     } else if (mode === 2){
         document.getElementById("signingChange").textContent = "使用手機登入";
         document.getElementById("signingMode").textContent = "常用信箱";
 
         document.getElementById("inputMode").placeholder="輸入信箱";
+
+        document.getElementById("newSelect").remove();
+        document.getElementById("inputMode").style.width="100%";
+
+
         mode = 1;
     }
 }
